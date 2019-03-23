78f07cbaf5acb2b232d916b66ed51cc1f3914e42
Ewa <efciajed@gmail.com>
2019-03-23 15:41:17 +0100

    hot fix: @NoArgConstructor in AnswerDTO

--- src/main/java/io/github/fixitlater/quizapi/question/AnswerDTO.java
+++ src/main/java/io/github/fixitlater/quizapi/question/AnswerDTO.java
@@ -3,11 +3,13 @@ package io.github.fixitlater.quizapi.question;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 class AnswerDTO {
     private int answerNo;
     private String answerBody;
