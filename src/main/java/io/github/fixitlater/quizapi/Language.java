41ab891171aa866485c58ba603cda97e36a9f0d5
piotr <pietia88@gmail.com>
2019-03-24 15:56:25 +0100

    corrected

--- src/main/java/io/github/fixitlater/quizapi/Language.java
+++ src/main/java/io/github/fixitlater/quizapi/Language.java
@@ -1,7 +1,15 @@
 package io.github.fixitlater.quizapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Language {
     POLISH,
     ENGLISH,
-    ANY
+    ANY;
+    public static List<Language> getPossibleValues(){
+        List<Language> languages = Arrays.asList(Language.values());
+        languages.remove(Language.ANY);
+        return languages;
+    }
 }
