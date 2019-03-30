24859af5a0689bbd0876c5116e5338137c000bf8
piotr <pietia88@gmail.com>
2019-03-30 09:58:27 +0100

    added new categories available

--- src/main/java/io/github/fixitlater/quizapi/Category.java
+++ src/main/java/io/github/fixitlater/quizapi/Category.java
@@ -7,5 +7,20 @@ import java.util.stream.Collectors;
 public enum Category {
     ANY,
     ENGLISH,
-    MULTIPLICATION
+    MULTIPLICATION,
+    TECHNOLOGY,
+    FOOD_AND_DRINKS,
+    HISTORY,
+    GEOGRAPHY,
+    BUSINESS_AND_POLITICS,
+    MOVIES,
+    MUSIC,
+    ART,
+    SPORT,
+    ENTERTAINMENT,
+    LITERATURE,
+    COMPUTER_GAMES,
+    RELIGIONS_AND_TRADITION,
+    MEDICINE,
+    NATURE
 }
